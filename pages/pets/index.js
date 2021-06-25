2d0fa2140ee63307d70c5fbfd019ffb90865302a
Paulo Neto <paulolemosfn@gmail.com>
2021-06-25 05:12:58 -0300

    Commit de pequenas atualizações no CSS

--- pages/pets/index.js
+++ pages/pets/index.js
@@ -4,8 +4,11 @@ import Link from 'next/link'
 function PetsPage() {
   return <>
     <Header />
-    <div>
+    <div class="add-pet">
       <h2>Lista dos seus Pets</h2>
+      <Link href="/pets/create">
+      <button>Adicione um pet</button>
+      </Link>
       <Link href="/home">
         <button>Voltar para home</button>
       </Link>
