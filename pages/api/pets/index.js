713d356fca4a75aabeae13b147c64d5dc77768fd
Paulo Neto <paulolemosfn@gmail.com>
2021-07-28 21:39:09 -0300

    Criando API de adiçao dos pets

--- pages/api/pets/index.js
+++ pages/api/pets/index.js
@@ -1,6 +1,21 @@
 import database from '../../../database'
-export default function handler(req, res) {
+export default function handlerCreate(req, res) {
+
+  const { namePet, typePet, breedPet } = req.body;
+
+  // const petExists = database.pets.find(eachUser => eachUser.namePet === namePet);
+
+  // if (petExists) {
+  //     return res.status(400).json({
+  //         message: 'Pet já cadastrado!'
+  //     });
+  // };
+
+  if (namePet ==='' || typePet ==='' || breedPet ==='') {
+    return res.status(400).json({
+      message: 'Dados incompletos!'
+    });
+  }
 
   return res.json(database.pets);
-
 }
--- pages/pets/create.js
+++ pages/pets/create.js
@@ -1,7 +1,53 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router'
+import axios from 'axios';
 import Header from "../../components/header"
 import Link from 'next/link'
 
 function CreatePage() {
+
+  const [namePet, setNamePet] = useState('');
+  const [typePet, setTypePet] = useState('');
+  const [breedPet, setBreedPet] = useState('');
+  const router = useRouter();
+
+
+  async function petCreate() {
+
+
+    try {
+      // const response = await axios.post('/api/pets/',
+      //   {
+      //     namePet: namePet,
+      //     typePet: typePet,
+      //     breedPet: breedPet
+      //   });
+      alert('funcionando')
+        console.log("funcionando");
+
+
+      // router.push("/pets/create");
+
+    } catch (error) {
+      alert(error.response.data.message)
+    }
+  };
+
+  function alterationNamePet(event) {
+    event.preventDefault();
+    setNamePet(event.target.value);
+  }
+
+  function alterationTypePet(event) {
+    event.preventDefault();
+    setTypePet(event.target.value);
+  }
+
+  function alterationBreedPet(event) {
+    event.preventDefault();
+    setBreedPet(event.target.value);
+  }
+
   return <>
     <Header />
     <div>
@@ -9,13 +55,13 @@ function CreatePage() {
       <form>
 
         <label>Digite o nome do seu pet</label>
-        <input id="nome pet" type="text" placeholder="Digite aqui o nome do pet"></input>
+        <input id="nome pet" type="text" placeholder="Digite aqui o nome do pet" onChange={alterationNamePet}></input>
         <label>Digite aqui o tipo do seu pet</label>
-        <input id="tipo pet" type="text" placeholder="Digite aqui o tipo do seu pet"></input>
+        <input id="tipo pet" type="text" placeholder="Digite aqui o tipo do seu pet" onChange={alterationTypePet}></input>
         <label>Digite aqui a raça do seu pet</label>
-        <input id="raça pet" type="text" placeholder="Digite aqui a raça do pet"></input>
+        <input id="raça pet" type="text" placeholder="Digite aqui a raça do pet" onChange={alterationBreedPet}></input>
 
-        <button>Adicionar</button>
+        <button onClick={petCreate}>Adicionar</button>
       </form>
       <Link href="/pets">
         <button>Voltar para os seus pets</button>
