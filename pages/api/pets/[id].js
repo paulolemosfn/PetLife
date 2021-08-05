b8e532bb0cb56a66eab51729fc3ede06f53acb15
Paulo Neto <paulolemosfn@gmail.com>
2021-08-04 22:01:46 -0300

    Corrigindo implementaçao da api de get de um  pet

--- pages/api/pets/[id].js
+++ pages/api/pets/[id].js
@@ -1,26 +1,9 @@
 import Database from '../../../db/config'
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import axios from 'axios';
-
-
 export default async function handler(req, res) {
 
   const db = await Database();
-  // const { id } = req.query;
-  const name = await db.get(`select name from pets where name = ${namePet}`);
-  
-  
-  const [pets, setPets] = useState([]);
-  
-  useEffect(async () => {
-    try {
-      const pets = await axios.get('/api/pets');
-      setPets(pets.data);
-    } catch (error) {
-      
-    }
-  });
-  
+  const { id } = req.query
+  const pet = await db.get(`select * from pets where id = ${id}`);
   return res.json(pet);
+
 }
